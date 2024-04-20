2f6bf463e61d8f2c039dec2d09bea7fcfb580cc2
NielJS <nnoelsantos@gmail.com>
2024-04-20 15:47:07 -0300

    fix cart pg

--- cart.js
+++ cart.js
@@ -1,11 +1,15 @@
-let data = await fetch("products.json")
-let products = await data.json()
-let cart = JSON.parse(localStorage.getItem("cart") || "[]")
-let cart_products = products.filter(({ id }) => cart.includes(id))
+async function list_cart() {
+    let data = await fetch("products.json")
+    let products = await data.json()
+    let cart = JSON.parse(localStorage.getItem("cart") || "[]")
+    let cart_products = products.filter(({ id }) => cart.includes(id))
 
-console.log("Products: ", products)
-console.log("Cart: ", cart)
-console.log("Cart Products: ", cart_products)
+    console.log("Products: ", products)
+    console.log("Cart: ", cart)
+    console.log("Cart Products: ", cart_products)
 
-cart_products.forEach(product =>
-    document.querySelector("#cart").innerHTML = `<div><b>Comprar:</b> ${product.name}</div>`)
+    cart_products.forEach(product =>
+        document.querySelector("#cart").innerHTML = `<div><b>Comprar:</b> ${product.name}</div>`)
+}
+
+list_cart()
