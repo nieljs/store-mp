9212816eb941075c9e61fcd16505f9e8c4ac3dee
NielJS <nnoelsantos@gmail.com>
2024-04-20 15:37:42 -0300

    fix cart data

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ async function go_to_cart() {
     let data = await fetch("products.json")
     let products = await data.json()
     let cart = JSON.parse(localStorage.getItem("cart") || "[]")
-    let cart_products = products.filter(id => cart.includes(id))
+    let cart_products = products.filter(({ id }) => cart.includes(id))
 
     console.log("Products: ", products)
     console.log("Cart: ", cart)
