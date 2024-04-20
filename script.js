9ec84e05bd8822f833b661a8108863e1b2aeea2c
NielJS <nnoelsantos@gmail.com>
2024-04-20 12:58:05 -0300

    products

---
+++ script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("products.json")
+        .then(response => response.json())
+        .then(products => {
+            let product_list = document.querySelector("#product-list")
+
+            products.forEach(product => {
+                let product_item = document.createElement("div")
+
+                product_item.innerHTML = `
+                    <img src="${product.image}"/>
+                    <h2>${product.name}</h2>
+                    <button>Add ao Carrinho</button>
+                `
+            })
+
+            product_list.appendChild(product_item)
+        })
+        .catch(err => console.error(err))
+})
