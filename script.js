6ad4e216467e0120d93dc0a4e62a5d53197c5116
NielJS <nnoelsantos@gmail.com>
2024-04-20 13:02:55 -0300

    fix siz img

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
                 let product_item = document.createElement("div")
 
                 product_item.innerHTML = `
-                    <img src="${product.image} width='500' height='500'"/>
+                    <img src="${product.image}" width="500" height="500"/>
                     <h2>${product.name}</h2>
                     <button>Add ao Carrinho</button>
                 `
